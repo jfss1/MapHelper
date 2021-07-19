9f258c748671e16b078a61b8f3f66c153baeadce
jfss1 <jose.filipe.sampaio@live.com.pt>
2021-07-19 18:34:47 +0100

    In MainActivity.kt, have a little problem with a Intent, who can't use Anotacao::class.java

---
+++ app/src/main/java/intro/android/cm/Anotacao.kt
@@ -0,0 +1,13 @@
+package intro.android.cm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Anotacao : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anotacao)
+
+        
+    }
+}
